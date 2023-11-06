4df84beebb51e9928d3ddda22f980904349892d2
Ibnu Choldun <ibnucholdun023@gmail.com>
2023-11-06 11:55:07 +0700

    create deposit balance and withdraw balance

--- src/routes/bankAccount.route.js
+++ src/routes/bankAccount.route.js
@@ -6,13 +6,20 @@ const {
   getBankAccontById,
   updateBankAccounts,
   deleteBankAccount,
+  depositBalance,
+  withdrawBalance,
 } = require("../controllers/bank_account.controller");
-const { CheckPostAccountReq } = require("../middleware/middleware");
+const {
+  CheckPostAccountReq,
+  CheckDepositWithdraw,
+} = require("../middleware/middleware");
 
 router.post("/", CheckPostAccountReq, createBankAccount);
 router.get("/", getAllBankAccounts);
 router.get("/:accountsId", getBankAccontById);
 router.put("/:accountsId", updateBankAccounts);
 router.delete("/:accountsId", deleteBankAccount);
+router.put("/deposit/:accountsId", CheckDepositWithdraw, depositBalance);
+router.put("/withdraw/:accountsId", CheckDepositWithdraw, withdrawBalance);
 
 module.exports = router;
